45eeab78d67fe673fe99fc38c1b866d5d95518c0
leonardovin <mcleozin4u@gmail.com>
2024-10-08 08:44:59 -0300

    feat: guest routes

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+import {PrismaAdapter} from '@next-auth/prisma-adapter'
+import { PrismaClient } from "@prisma/client";
+
+
+
+const GOOGLE_CLIENT_ID =  process.env.GOOGLE_CLIENT_ID || ''
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || ''
+const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET || ''
+
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET
+    })
+  ],
+  adapter: PrismaAdapter(new PrismaClient()),
+  secret: NEXTAUTH_SECRET,
+  callbacks: {
+    async session({session, token, user}) {
+      session.user.id = user.id
+      return session
+    }
+  }
+})
